596b9d42702afa8e148b5ca76df8f866515e19c6
Matt Welland <mattwellie@gmail.com>
2016-05-04 13:13:23 +0100

    Update query_builder.py

--- query_builder.py
+++ query_builder.py
@@ -1,24 +1,70 @@
-# Script to build queries based on results from variants passing filters
+#!/bin/bash
+for i in "$@"
+    do
+    case $i in
+        # The file containing the results of the impc batch query
+        -f=*|--filein=*)
+        impc="${i#*=}"
+        ;;
+        
+        # The file containing the results of the clinical filtering script
+        -c=*|--clinicalfilter=*)
+        clin="${i#*=}"
+        ;;
+        
+        # This will be 1 if you have parks gene list files to condense, otherwise this is skipped
+        -p=*|--parks=*)
+        parks="${i#*=}"
+        ;;
 
-import sys
+        # The intersection of the gene lists with the clinical filtering output
+        -filter=*)
+        filterout="${i#*=}"
+        ;;
 
-in_file = sys.argv[1]  
-queries_out = sys.argv[2]
-template = 'zgrep {} /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/{}/{}/{}/vcf/{}.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep {}'
+        # The name of a Bash script which will be written to query the original VCFs
+        -q=*|--query=*)
+        query="${i#*=}"
+        ;;
+        
+        # The name of a file which will be used to contain the zgrep results from database
+        -qr=*)
+        queryresults="${i#*=}"
+        ;;
+        
+    esac
+done
 
-with open(in_file, 'r') as handle:
-    with open(queries_out, 'w') as outhandle:
-        for line in handle:
-            if '----------' in line: 
-                filename = line.split('----------')[1]
-                print >>outhandle, 'echo "{}"'.format(filename.rstrip())
-            else:
-                list = line.split('\t')
-                if list[0] == 'proband': continue
-                else:
-                    PP = list[0]
-                    pp1 = PP[2:4]
-                    pp2 = PP[4:]
-                    pos = list[4]
-                    gene = list[5]
-                    print >>outhandle, template.format(gene, pp1, pp2, PP, PP, pos)
+# Run this to check if the file containing the impc batch results is there
+# Default action is ignore this command
+if [ -f "$impc" ]
+then
+    python batch_process.py -filein $impc
+fi
+
+# If the parks argument is 1
+if [ "$parks" = "1" ]
+then
+    python pickle_my_parks.py
+fi
+
+# To tidy up all that mess... Convert pickles to text lists
+# This can be run whether the previous steps were run or not
+# Makes the final outputs available to perl or python
+python pickle_to_genelist.py
+
+# At this point you should probably have run the clinical filtering script on an appropriate cohort... The name of that output file should be the argument -filter -> $filterout
+
+# This step scans the clinical filtering output against the lists of gene names
+# This will be a text file delimited by lines of '----------filename.txt' identifying which results correspond to which gene lists
+python python_scan.py $clin genelist_* > $filterout
+
+# Based on the output of the intersection script, this will build queries on the original VCFs
+# From the lines which passed clinical filtering, construct zgrep queries to get full annotation
+python query_builder.py  $filterout  $query
+
+# Run that shell script, sending all outcomes of the zgrep commands to a text file (arg)
+bash $query > $queryresults
+
+# Run a script to pair up all variants passing filter with their allele freqs
+python match.py $queryresults $clin
