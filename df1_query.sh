cb61aab7d21796b404497b442223281bad9cd1f5
Matt Welland <mattwellie@gmail.com>
2016-05-05 15:25:26 +0100

    Create df1_query.sh

---
+++ df1_query.sh
@@ -0,0 +1,49 @@
+echo "genelist_impc.txt"
+echo -n -e "PP0315	SCN4A	"; zgrep SCN4A /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/03/15/PP0315/vcf/PP0315.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 62022122
+echo -n -e "PP0004	LAMA5	"; zgrep LAMA5 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/04/PP0004/vcf/PP0004.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 60892813
+echo -n -e "PP0004	LAMA5	"; zgrep LAMA5 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/04/PP0004/vcf/PP0004.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 60886088
+echo -n -e "PP0025	ATP6V1B2	"; zgrep ATP6V1B2 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/25/PP0025/vcf/PP0025.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 20069263
+echo -n -e "PP0267	DHX33	"; zgrep DHX33 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/02/67/PP0267/vcf/PP0267.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 5365725
+echo -n -e "PP0065	AGAP1	"; zgrep AGAP1 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/65/PP0065/vcf/PP0065.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 236839424
+echo -n -e "PP0267	CLSPN	"; zgrep CLSPN /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/02/67/PP0267/vcf/PP0267.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 36216977
+echo -n -e "PP0273	KIF26B	"; zgrep KIF26B /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/02/73/PP0273/vcf/PP0273.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 245851740
+echo -n -e "PP0273	KIF26B	"; zgrep KIF26B /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/02/73/PP0273/vcf/PP0273.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 245766015
+echo -n -e "PP0267	DNAJC16	"; zgrep DNAJC16 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/02/67/PP0267/vcf/PP0267.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 15894604
+echo -n -e "PP0303	CHRNA1	"; zgrep CHRNA1 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/03/03/PP0303/vcf/PP0303.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 175614789
+echo -n -e "PP0303	CHRNA1	"; zgrep CHRNA1 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/03/03/PP0303/vcf/PP0303.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 175619069
+echo "genelist_lethal.txt"
+echo -n -e "PP0062	TTN	"; zgrep TTN /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/62/PP0062/vcf/PP0062.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 179454530
+echo -n -e "PP0010	TTN	"; zgrep TTN /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/10/PP0010/vcf/PP0010.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 179430460
+echo -n -e "PP0010	TTN	"; zgrep TTN /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/10/PP0010/vcf/PP0010.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 179497758
+echo -n -e "PP0047	TTN	"; zgrep TTN /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/47/PP0047/vcf/PP0047.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 179452447
+echo -n -e "PP0047	TTN	"; zgrep TTN /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/47/PP0047/vcf/PP0047.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 179611552
+echo -n -e "PP0010	TTN	"; zgrep TTN /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/10/PP0010/vcf/PP0010.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 179579172
+echo -n -e "PP0062	TTN	"; zgrep TTN /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/62/PP0062/vcf/PP0062.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 179610967
+echo -n -e "PP0062	TTN	"; zgrep TTN /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/62/PP0062/vcf/PP0062.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 179424272
+echo -n -e "PP0062	TTN	"; zgrep TTN /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/62/PP0062/vcf/PP0062.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 179404498
+echo -n -e "PP0013	NF1	"; zgrep NF1 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/13/PP0013/vcf/PP0013.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 29562669
+echo -n -e "PP0053	SMPD1	"; zgrep SMPD1 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/53/PP0053/vcf/PP0053.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 6411935
+echo -n -e "PP0062	FGFR3	"; zgrep FGFR3 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/62/PP0062/vcf/PP0062.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 1806099
+echo -n -e "PP0053	COL2A1	"; zgrep COL2A1 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/53/PP0053/vcf/PP0053.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 48369853
+echo -n -e "PP0059	NEB	"; zgrep NEB /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/59/PP0059/vcf/PP0059.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 152384078
+echo -n -e "PP0059	NEB	"; zgrep NEB /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/59/PP0059/vcf/PP0059.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 152346522
+echo "genelist_nondisease_lethal.txt"
+echo -n -e "PP0065	SMARCC1	"; zgrep SMARCC1 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/65/PP0065/vcf/PP0065.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 47727627
+echo -n -e "PP0004	LAMA5	"; zgrep LAMA5 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/04/PP0004/vcf/PP0004.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 60892813
+echo -n -e "PP0004	LAMA5	"; zgrep LAMA5 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/04/PP0004/vcf/PP0004.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 60886088
+echo -n -e "PP0013	RERE	"; zgrep RERE /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/13/PP0013/vcf/PP0013.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 8418331
+echo -n -e "PP0013	RERE	"; zgrep RERE /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/13/PP0013/vcf/PP0013.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 8418909
+echo -n -e "PP0025	MACF1	"; zgrep MACF1 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/25/PP0025/vcf/PP0025.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 39901245
+echo -n -e "PP0025	MACF1	"; zgrep MACF1 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/25/PP0025/vcf/PP0025.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 39851427
+echo "genelist_nondisease_nonlethal.txt"
+echo -n -e "PP0025	SEMA4D	"; zgrep SEMA4D /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/25/PP0025/vcf/PP0025.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 91994007
+echo -n -e "PP0019	DLG2	"; zgrep DLG2 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/19/PP0019/vcf/PP0019.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 84028095
+echo -n -e "PP0019	CD244	"; zgrep CD244 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/19/PP0019/vcf/PP0019.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 160811672
+echo "genelist_nondisease_postlethal.txt"
+echo "genelist_nonlethal.txt"
+echo -n -e "PP0004	GRIN2A	"; zgrep GRIN2A /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/04/PP0004/vcf/PP0004.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 9857047
+echo "genelist_postlethal.txt"
+echo -n -e "PP0062	FGFR3	"; zgrep FGFR3 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/62/PP0062/vcf/PP0062.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 1806099
+echo -n -e "PP0059	NEB	"; zgrep NEB /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/59/PP0059/vcf/PP0059.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 152384078
+echo -n -e "PP0059	NEB	"; zgrep NEB /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/59/PP0059/vcf/PP0059.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 152346522
+echo -n -e "PP0053	SMPD1	"; zgrep SMPD1 /lustre/scratch115/realdata/mdt3/projects/pagedata/data_freezes/2015-09-03/PP/00/53/PP0053/vcf/PP0053.uber_vep_tabix_qc.2015-11-11.vcf.gz | grep 6411935
